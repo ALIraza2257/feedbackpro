0cf5519a20cf89d374d37de66c1eadb2b9907653
Ali Raza <79046902+ALIraza2257@users.noreply.github.com>
2024-03-08 16:32:19 +0500

    removed something

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,302 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+
+import { DecimalPipe, NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
+import { DashboardService } from 'app/modules/admin/dashboard/dashboard.service';
+import { ApexOptions, NgApexchartsModule } from 'ng-apexcharts';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector     : 'app-dashboard',
-  standalone   : true,
   templateUrl  : './dashboard.component.html',
-  encapsulation: ViewEncapsulation.None,
+  encapsulation  : ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone     : true,
+  imports        : [MatButtonModule, MatIconModule, MatMenuModule, MatButtonToggleModule, NgApexchartsModule, MatTooltipModule, NgFor, DecimalPipe],
 })
-export class DashboardComponent {
 
+export class DashboardComponent implements OnInit, OnDestroy
+{
+    chartVisitors: ApexOptions;
+    chartNewVsReturning: ApexOptions;
+    data: any;
+
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
+
+    /**
+     * Constructor
+     */
+    constructor(
+        private _analyticsService: DashboardService,
+        private _router: Router,
+    )
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void
+    {
+        // Get the data
+        this._analyticsService.data$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((data) =>
+            {
+                // Store the data
+                this.data = data;
+
+                // Prepare the chart data
+                this._prepareChartData();
+            });
+
+        // Attach SVG fill fixer to all ApexCharts
+        window['Apex'] = {
+            chart: {
+                events: {
+                    mounted: (chart: any, options?: any): void =>
+                    {
+                        this._fixSvgFill(chart.el);
+                    },
+                    updated: (chart: any, options?: any): void =>
+                    {
+                        this._fixSvgFill(chart.el);
+                    },
+                },
+            },
+        };
+    }
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void
+    {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Track by function for ngFor loops
+     *
+     * @param index
+     * @param item
+     */
+    trackByFn(index: number, item: any): any
+    {
+        return item.id || index;
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Private methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Fix the SVG fill references. This fix must be applied to all ApexCharts
+     * charts in order to fix 'black color on gradient fills on certain browsers'
+     * issue caused by the '<base>' tag.
+     *
+     * Fix based on https://gist.github.com/Kamshak/c84cdc175209d1a30f711abd6a81d472
+     *
+     * @param element
+     * @private
+     */
+    private _fixSvgFill(element: Element): void
+    {
+        // Current URL
+        const currentURL = this._router.url;
+
+        // 1. Find all elements with 'fill' attribute within the element
+        // 2. Filter out the ones that doesn't have cross reference so we only left with the ones that use the 'url(#id)' syntax
+        // 3. Insert the 'currentURL' at the front of the 'fill' attribute value
+        Array.from(element.querySelectorAll('*[fill]'))
+            .filter(el => el.getAttribute('fill').indexOf('url(') !== -1)
+            .forEach((el) =>
+            {
+                const attrVal = el.getAttribute('fill');
+                el.setAttribute('fill', `url(${currentURL}${attrVal.slice(attrVal.indexOf('#'))}`);
+            });
+    }
+
+    /**
+     * Prepare the chart data from the data
+     *
+     * @private
+     */
+    private _prepareChartData(): void
+    {
+        // Visitors
+        this.chartVisitors = {
+            chart     : {
+                animations: {
+                    speed           : 400,
+                    animateGradually: {
+                        enabled: false,
+                    },
+                },
+                fontFamily: 'inherit',
+                foreColor : 'inherit',
+                width     : '100%',
+                height    : '100%',
+                type      : 'area',
+                toolbar   : {
+                    show: false,
+                },
+                zoom      : {
+                    enabled: false,
+                },
+            },
+            colors    : ['#818CF8'],
+            dataLabels: {
+                enabled: false,
+            },
+            fill      : {
+                colors: ['#312E81'],
+            },
+            grid      : {
+                show       : true,
+                borderColor: '#334155',
+                padding    : {
+                    top   : 10,
+                    bottom: -40,
+                    left  : 0,
+                    right : 0,
+                },
+                position   : 'back',
+                xaxis      : {
+                    lines: {
+                        show: true,
+                    },
+                },
+            },
+            series    : this.data.visitors.series,
+            stroke    : {
+                width: 2,
+            },
+            tooltip   : {
+                followCursor: true,
+                theme       : 'dark',
+                x           : {
+                    format: 'MMM dd, yyyy',
+                },
+                y           : {
+                    formatter: (value: number): string => `${value}`,
+                },
+            },
+            xaxis     : {
+                axisBorder: {
+                    show: false,
+                },
+                axisTicks : {
+                    show: false,
+                },
+                crosshairs: {
+                    stroke: {
+                        color    : '#475569',
+                        dashArray: 0,
+                        width    : 2,
+                    },
+                },
+                labels    : {
+                    offsetY: -20,
+                    style  : {
+                        colors: '#CBD5E1',
+                    },
+                },
+                tickAmount: 20,
+                tooltip   : {
+                    enabled: false,
+                },
+                type      : 'datetime',
+            },
+            yaxis     : {
+                axisTicks : {
+                    show: false,
+                },
+                axisBorder: {
+                    show: false,
+                },
+                min       : (min): number => min - 750,
+                max       : (max): number => max + 250,
+                tickAmount: 5,
+                show      : false,
+            },
+        };
+
+        // New vs. returning
+        this.chartNewVsReturning = {
+            chart      : {
+                animations: {
+                    speed           : 400,
+                    animateGradually: {
+                        enabled: false,
+                    },
+                },
+                fontFamily: 'inherit',
+                foreColor : 'inherit',
+                height    : '100%',
+                type      : 'donut',
+                sparkline : {
+                    enabled: true,
+                },
+            },
+            colors     : ['#6aff00','#3182CE', '#63B3ED','#63B3ED','#63B3ED'],
+            labels     : this.data.newVsReturning.labels,
+            plotOptions: {
+                pie: {
+                    customScale  : 0.9,
+                    expandOnClick: false,
+                    donut        : {
+                        size: '70%',
+                    },
+                },
+            },
+            series     : this.data.newVsReturning.series,
+            states     : {
+                hover : {
+                    filter: {
+                        type: 'none',
+                    },
+                },
+                active: {
+                    filter: {
+                        type: 'none',
+                    },
+                },
+            },
+            tooltip    : {
+                enabled        : true,
+                fillSeriesColor: false,
+                theme          : 'dark',
+                custom         : ({
+                    seriesIndex,
+                    w,
+                }): string => `<div class="flex items-center h-8 min-h-8 max-h-8 px-3">
+                                                    <div class="w-3 h-3 rounded-full" style="background-color: ${w.config.colors[seriesIndex]};"></div>
+                                                    <div class="ml-2 text-md leading-none">${w.config.labels[seriesIndex]}:</div>
+                                                    <div class="ml-2 text-md font-bold leading-none">${w.config.series[seriesIndex]}%</div>
+                                                </div>`,
+            },
+        };
+
+
+
+
+
+
+    }
 }
--- src/app/modules/admin/dashboard/dashboard.routes.ts
+++ src/app/modules/admin/dashboard/dashboard.routes.ts
@@ -1,9 +1,14 @@
+import { inject } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { Routes } from '@angular/router';
+import { DashboardService } from 'app/modules/admin/dashboard/dashboard.service';
 
 export default [
     {
         path     : '',
         component: DashboardComponent,
+        resolve  : {
+            data: () => inject(DashboardService).getData(),
+        },
     },
 ] as Routes;
